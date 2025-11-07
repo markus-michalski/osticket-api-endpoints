347eb12d06c31e043a9f3a03789c6f43acff14ae
Markus Michalski <ich@markus-michalski.net>
2025-11-07 17:10:33 +0100

    chore: Bump version to 1.2.1 for deployment

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 return array(
     'id' =>             'com.osticket:api-endpoints',
-    'version' =>        '1.2.0',
+    'version' =>        '1.2.1',
     'name' =>           'API Endpoints',
     'author' =>         'Markus Michalski',
     'description' =>    'Extends osTicket API with additional endpoints and parameters: ticket creation with Markdown support, department selection, and subticket functionality.',
